42c1fb241aac76118ceb97380cb454c573e43c18
johnpyp <johnpyp.dev@gmail.com>
2021-10-13 15:20:16 -0400

    Add PageWrapper to client

---
+++ packages/client/src/components/PageWrapper.tsx
@@ -0,0 +1,13 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+
+export interface PageWrapperProps {
+  variant?: "sm";
+}
+export const PageWrapper: React.FC<PageWrapperProps> = ({ children, variant }) => {
+  return (
+    <Flex justify="center" p="8" w={variant === "sm" ? "sm" : "full"}>
+      {children}
+    </Flex>
+  );
+};
